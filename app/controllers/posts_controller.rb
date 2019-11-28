292ece979d67e584892ec4d1c6d18271a890dee9
fasnimo <fasnimo@gmail.com>
2019-11-28 11:06:34 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
-    @author = @post.author
+    #This line below can be deleted because of partial in 'authors/author' we used <%= render partial: "authors/author", locals: {post_author: @post.author} %>
+    # @author = @post.author
   end
 
   def index
